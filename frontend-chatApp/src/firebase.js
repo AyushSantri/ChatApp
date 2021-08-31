361bf26c8a14bdced35258e4b0ba0585099c7d9a
Ayush <ayushsantri222@gmail.com>
2021-09-01 00:13:56 +0530

    frontend

---
+++ frontend-chatApp/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDm8YMQbJhjkUhqxYLKeJkto4zyGW7iv3k",
+    authDomain: "whatsapp-mern-98f21.firebaseapp.com",
+    projectId: "whatsapp-mern-98f21",
+    storageBucket: "whatsapp-mern-98f21.appspot.com",
+    messagingSenderId: "425973391191",
+    appId: "1:425973391191:web:408dece3c6becb6da66812"
+  };
